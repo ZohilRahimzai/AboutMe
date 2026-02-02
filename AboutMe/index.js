9946e1bea463256c6271b71fbe0a48fb23fe4017
Zohil Rahimzai <zohilkhan2026@gmail.com>
2026-02-02 11:36:58 +0430

    Updated

--- AboutMe/index.js
+++ AboutMe/index.js
@@ -1,6 +1,6 @@
 let Btnone = document.getElementById("btnone");
 let Hthree = document.getElementById("hthree");
-let Sdev = document.getElementById("ZL");
+let Sdev = document.getElementById("x");
 
 
 
@@ -9,5 +9,16 @@ function btnone() {
     
     Btnone.innerText = "Button clicked!!!";
     Hthree.innerText = "Welcome!!!";
+    Sdev.innerHTML=` <div id="sdiv">
+            <h1 id="name">Zohil Rahimzai</h1>
+            <hr>
+            <img id="img1" src="Z.png">
+            <h2 id="hone">Software Engineer | FullStack Developer<br><br>
+            My portFolio Website:<a href="https://zohil.me/"> Zohil.me </a> <br><br>
+            X:<a href="@Zohil Rahimzai"> Zohil Rahimzai</a> <br><br>
+            GitHub: <a href="https://github.com/ZohilRahimzai"> Zohil Rahimzai </a> <br><br>
+            EmailAddress: hi@zohil.me   
+
+        </h2>  </div>`
 
 }
